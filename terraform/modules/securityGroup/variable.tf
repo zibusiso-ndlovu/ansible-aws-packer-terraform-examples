7d9933444ea84a3c71c89767503cdcc62339df9d
Zibusiso Edwin Ndlovu <ndlovuzibusiso@gmail.com>
2021-01-26 13:12:34 +0200

    Ädded Ansible, packer and terraform modules

---
+++ terraform/modules/securityGroup/variable.tf
@@ -0,0 +1,13 @@
+
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "vpc_id" {
+  description = "VPC id"
+  default = ""
+}
+variable "environment_tag" {
+  description = "Environment tag"
+  default = ""
+}
